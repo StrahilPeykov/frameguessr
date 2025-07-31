b140d0247e39f91c20f28cba1bba642a1ee49918
Strahil Peykov <strahil.peykov@gmail.com>
2025-07-31 19:00:44 +0200

    Update index.ts

--- hooks/index.ts
+++ hooks/index.ts
@@ -4,10 +4,3 @@ export { useDailyChallenge } from './useDailyChallenge'
 export { useAudioHints } from './useAudioHints'
 export { useAuth } from './useAuth'
 export { useNavigation } from './useNavigation'
-
-// Default exports
-export { default as useGameStateDefault } from './useGameState'
-export { default as useDailyChallengeDefault } from './useDailyChallenge'
-export { default as useAudioHintsDefault } from './useAudioHints'
-export { default as useAuthDefault } from './useAuth'
-export { default as useNavigationDefault } from './useNavigation'
